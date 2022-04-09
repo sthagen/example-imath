e82e91b1e3d521230089ac24c06ff2099ae5c016
Stefan Hagen <stefan@hagen.link>
2022-04-09 18:52:27 +0200

    Fixed completely failing example from https://imath.readthedocs.io/en/latest/intro.html#example

--- example/main.cpp
+++ example/main.cpp
@@ -1,4 +1,5 @@
-#include <Imath/ImathVec.h>
+#include <ImathMatrix.h>
+#include <ImathVec.h>
 #include <iostream>
 
 int
@@ -6,13 +7,21 @@ main()
 {
     Imath::V3f v (3.0, 4.0, 5.0);
 
+    std::cout << "What's your raw vector v, Rachid? " << v << std::endl;
     v.normalize();
 
     Imath::M33f M;
-    M.translate (1.0, 2.0, 3.0);
+    std::cout << "What's your raw matrix M, Roberta? \n" << M << std::endl;
+    Imath::V2f t{1.0, 2.0};
+    std::cout << "What's your vector t, Tamid? " << t << std::endl;
+    M.translate(t);
     Imath::V3f p = v * M;
 
-    std::cout << "What's your vector, Victor? " << p << std::endl;
+    std::cout << "What's your vector p, Pocahontas? " << p << std::endl;
+
+    std::cout << "What's your vector v, Victor? " << v << std::endl;
+    std::cout << "What's your matrix M, Martha? \n" << M << std::endl;
+
 
     return 0;
 }
