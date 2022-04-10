eb3ebd5371a220729f5a3c562b64d8c404267fa4
Stefan Hagen <stefan@hagen.link>
2022-04-10 08:28:43 +0200

    Followed suggestion of @lgritz in https://github.com/AcademySoftwareFoundation/Imath/issues/251#issuecomment-1094138251

--- example/main.cpp
+++ example/main.cpp
@@ -1,18 +1,20 @@
+#include <iostream>
 #include <ImathMatrix.h>
 #include <ImathVec.h>
-#include <iostream>
 
 int
 main()
 {
-    Imath::V3f v (3.0, 4.0, 5.0);
+    Imath::V3f v{3.f, 4.f, 5.f};
 
     v.normalize();
 
-    Imath::M33f M;
-    Imath::V2f t{1.0, 2.0};
+    Imath::M44f M;
+    Imath::V3f const t{1.f, 2.f, 3.f};
     M.translate(t);
-    Imath::V3f p = v * M;
+
+    Imath::V3f p;
+    M.multVecMatrix(v, p);
 
     std::cout << "What's your vector, Victor? " << p << std::endl;
 
